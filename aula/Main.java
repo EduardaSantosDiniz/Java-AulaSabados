a59d176891723b1253efb730449138cbc9119067
Eduarda Santos Diniz <eduardafaculdade0306@gmail.com>
2024-03-16 11:00:36 -0300

    finalizando

---
+++ aula/Main.java
@@ -0,0 +1,14 @@
+package aula;
+
+public class Main {
+    public static void main(String[] args) {
+
+    
+        Calculadora calc = new Calculadora();
+        calc.numA = 2.0;
+        calc.numB = 3.0;
+        System.out.println("Soma: "+ calc.somar());
+   
+
+}
+}
---
+++ aula/calculadora.java
@@ -0,0 +1,10 @@
+package aula;
+
+public class Calculadora{
+    Double numA;
+    Double numB;
+
+    double somar(){
+        return numA + numB;
+    }
+}
